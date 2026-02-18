b88eff62fab5f241adc3cf2222e53c5723920e86
Aditya Sadalagi <adityasadalagi10@gmail.com>
2026-02-18 23:27:27 +0530

    Created basic requirements

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    return <footer>
+        <p>Made with ❤️ by Aditya | Copyrights © {new Date().getFullYear()}</p>
+    </footer>;
+}
+
+export default Footer;
